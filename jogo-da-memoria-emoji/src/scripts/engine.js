a0df31f9b2abe212df90b04e15ee150c36924191
Catarina Coelho <k.tarina56@gmail.com>
2024-11-01 13:21:08 -0300

    update files
    -create engine.js
    -update main.css

---
+++ jogo-da-memoria-emoji/src/scripts/engine.js
@@ -0,0 +1,12 @@
+const emojis = ["🍀","🍀","🌻","🌻","👽","👽","🐧","🐧","🤡","🤡","🫶","🫶","⏰","⏰","🍒","🍒"];
+let openCards= [];
+
+let shuffleEmojis = emojis.sort(()=>(Math.random() > 0.5 ? 2: -1));
+
+for(let i=0; i < emojis.length; i++){
+    let box = document.createElement
+    ("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
