e9a8655045349b26bbe759b1bac901c083b06ea4
Catarina Coelho <k.tarina56@gmail.com>
2025-04-11 13:47:06 -0300

    tailwind

--- to-do-react/src/App.jsx
+++ to-do-react/src/App.jsx
@@ -1,9 +1,11 @@
+import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
 function App() {
   return (
     <div>
-      <h1>Gerenciador de Tarefas</h1>
+      <h1 className="text-red-500">Gerenciador de Tarefas</h1>
+      <AddTask />
       <Tasks />
     </div>
   );
