cc429f177b89662638264542a08e3a00654e5ffe
Catarina Coelho <k.tarina56@gmail.com>
2025-04-23 14:55:32 -0300

    create tasks

--- to-do-react/src/App.jsx
+++ to-do-react/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
 
@@ -6,18 +7,16 @@ function App() {
     { id: 1, title: "Task 1", description: "tralalá", isCompleted: false },
     { id: 2, title: "Task 2", description: "tralalá2", isCompleted: false },
     { id: 3, title: "Task 3", description: "tralalá3", isCompleted: false },
-    { id: 4, title: "Task 4", description: "tralalá4", isCompleted: false },
-    { id: 5, title: "Task 5", description: "tralalá5", isCompleted: false },
   ]);
 
   return (
-    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+    <div className="w-screen h-screen bg-indigo-500 flex justify-center p-6">
       <div className="w-[500px]">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">
+        <h1 className="text-3xl text-indigo-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
         <AddTask />
-        <Tasks tasks={tasks}/>
+        <Tasks tasks={tasks} />
       </div>
     </div>
   );
--- to-do-react/src/components/Tasks.jsx
+++ to-do-react/src/components/Tasks.jsx
@@ -1,5 +1,16 @@
 function Tasks(props) {
-  return <h1>{props.tasks[0].title}</h1>;
+  return (
+    <ul className="space-y-4 p-6 bg-indigo-200 rounded-md shadow">
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex">
+          <button className=" bg-indigo-400 w-full text-white p-2 rounded-md">
+            {task.title}
+          </button>
+          <button>ver detalhes</button>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default Tasks;
